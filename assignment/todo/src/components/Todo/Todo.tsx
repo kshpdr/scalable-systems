a6e09959d7ac30d21009e96202defdeebf5ebcd2
strfre <alekseev.stfre@gmail.com>
2023-05-16 18:25:08 +0200

    Add checkbox functionality

--- assignment/todo/src/components/Todo/Todo.tsx
+++ assignment/todo/src/components/Todo/Todo.tsx
@@ -1,6 +1,7 @@
 import { Button, CloseButton, Form, ProgressBar, Badge } from "solid-bootstrap";
 import { Component, createSignal } from "solid-js";
 import { Buttons, TodoContainer } from "./Todo.styles";
+import { editTodo } from "../../api/todosApi";
 
 interface TodoProps {
     id: string;
@@ -11,11 +12,16 @@ interface TodoProps {
     onDelete: (id: string) => void;
 }
 
+const handleCheckbox = async (id: string, data: {task: string, deadline: string, done: boolean, progress: number }) => {
+    data.done = !data.done
+    await editTodo(id, data)
+}
+
 const Todo: Component<TodoProps> = ({ id, task, done, progress, deadline, onDelete}) => {
 
     return (
         <TodoContainer>
-            <Form.Check type={"checkbox"} checked={done}/>
+            <Form.Check type={"checkbox"} checked={done} onclick={() => handleCheckbox(id, {task, done, progress, deadline})}/>
             {task}
             <ProgressBar now={10} label={`${progress}%`} style={{ width: "350px" }} />
             <Badge pill bg="primary">{deadline}</Badge>
--- assignment/todo/src/pages/Homepage/Homepage.tsx
+++ assignment/todo/src/pages/Homepage/Homepage.tsx
@@ -4,10 +4,11 @@ import {tasks} from '../../tasks.js'
 import { Button } from 'solid-bootstrap';
 import { AddButton } from './Homepage.styles.jsx';
 import { getAllTodos } from '../../api/todosApi';
-import { deleteTodo } from '../../api/todosApi';
+import { deleteTodo, editTodo } from '../../api/todosApi';
 
 const Homepage: Component<{}> = () => {
     const [todos, setTodos] = createSignal([] as any[]);
+    
 
     createEffect(() => {
         const fetchData = async () => {
