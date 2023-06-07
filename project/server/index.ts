a57e0eeec66b56e61e1e7c196537770877ce16e1
strfre <alekseev.stfre@gmail.com>
2023-06-07 10:28:31 +0200

    Add db connections, cluster model, two basic functions, which dont work without proper routing system, i guess

--- project/server/index.ts
+++ project/server/index.ts
@@ -1,18 +1,31 @@
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
+import { clusters, getclust } from './models/clusters';
+import { sequelize } from './db'
 
 const app: express.Application = express();
 
 app.use(cors());
 
 const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3001;
-const clientBuildPath = path.resolve(__dirname, '..', 'client', 'dist');
+const clientBuildPath = path.resolve(__dirname, '..', 'client');
 
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
+const start = async () => {
+    try {
+        await sequelize.authenticate().then(() => {console.log("siuuuu")});
+        await sequelize.sync();
+    }
+    catch (err) {
+        console.log(err);
+    }
+}; 
+start();
+
 app.get('/api', (req, res) => {
-    res.send({ message: 'Welcome to our API!' });
+    res.send({ message: `Welcome to our API! ${getclust} \n \n ${JSON.stringify(getclust, null, 2)}` });
 });
 
 app.get('*', (req, res) =>{
