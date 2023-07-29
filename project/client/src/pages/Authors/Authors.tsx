584e196a7db3f6cffc171c556709add9f93eeb23
strfre <alekseev.stfre@gmail.com>
2023-07-29 13:47:14 +0200

    14 regions in about

--- project/client/src/pages/Authors/Authors.tsx
+++ project/client/src/pages/Authors/Authors.tsx
@@ -17,7 +17,7 @@ const Authors: React.FC = () => {
                     <div>
                         <h2>Our Project</h2> <br/>
                         In our project we allow potential users to create computing jobs with properties described in Jobs page in order to sort them 
-                        between computing clusters located in all 15 regions of Great Britain according to actual carbon intensity values in those regions. <br/>
+                        between computing clusters located in all 14 regions of Great Britain according to actual carbon intensity values in those regions. <br/>
                         Intensity data is fetched from <a href="carbonintensity.org.uk">carbonintensity.org.uk</a> <br/>
                         Jobs are then sorted after their dedline, and our algorithm then assignes timeslots to the jobs according to lowest intensity values for the next 48 hours. <br/>
 
